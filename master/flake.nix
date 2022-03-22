a91586e44b9a8982b2ff2faeba0ea559b648942e
hugosenari <hugosenari@gmail.com>
2022-03-22 00:37:20 -0300

    chore: re-index ../nimpkgs/s/sequtils2

--- master/flake.nix
+++ master/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Additional functions for sequences that are not present in sequtils'';
+  description = ''Additional methods for sequences that are not present in sequtils'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-sequtils2-master.flake = false;
   inputs.src-sequtils2-master.owner = "Michedev";
-  inputs.src-sequtils2-master.ref   = "refs/heads/master";
+  inputs.src-sequtils2-master.ref   = "master";
   inputs.src-sequtils2-master.repo  = "sequtils2";
   inputs.src-sequtils2-master.type  = "github";
   
